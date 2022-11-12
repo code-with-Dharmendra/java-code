2a840068b8a2a81dfe2dbda04926f74f2a3d0c42
code-with-Dharmendra <117857010+code-with-Dharmendra@users.noreply.github.com>
2022-11-12 13:21:01 +0530

    Add files via upload

---
+++ program_one.java
@@ -0,0 +1,16 @@
+/*
+     * More java Statements
+     * this compute computes square root
+     */
+import java.lang.Math;
+public class program_one {
+   public static void main(String args[])
+   {
+    double x = 5; //Declaration and initialization
+    double y;//Simple declaration
+    y = Math.sqrt(x) ;
+    System.out.println ("y = "+y);
+   }
+
+    
+}
